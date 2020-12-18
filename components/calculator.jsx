79d84ad8fdb98606b0cd121ae400efd95326e5d0
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2020-12-18 16:11:30 -0500

    Install all dependencies files and folder

---
+++ components/calculator.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+  <div className="page">
+    <div className="title">Simple Calculator</div>
+    <div className="subtitle">Enter two numbers to calculate</div>
+  </div>
+)
