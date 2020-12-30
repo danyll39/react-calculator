b800b15a177c0465f21e8624c9f2a89b528da833
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2020-12-30 17:46:32 -0500

    Add input boxes result box and calculate button

--- components/calculator.jsx
+++ components/calculator.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default () => (
-  <div className="page">
-    <div className="title">Simple Calculator</div>
-    <div className="subtitle">Enter two numbers to calculate</div>
-  </div>
-)
+export default () => {
+  const [number1, setNumber1] = useState('')
+  const [number2, setNumber2] = useState('')
+  const [result, setResult] = useState('')
+
+  return (
+    <div className="calculator">
+      <div className="title">Simple Calculator</div>
+      <div className="title">Enter two numbers to calculate</div>
+      <div className="form">
+        <input
+          type="text"
+          name="number1"
+          value={number1}
+          onChange={event => setNumber1(event.target.value)}
+        />
+
+        <input
+          type="text"
+          name="number2"
+          value={number2}
+          onChange={event => setNumber2(event.target.value)}
+        />
+        <button type="button">=</button>
+        <input value={result} readOnly />
+      </div>
+
+    </div>
+  )
+}
